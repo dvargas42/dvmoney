2acb8060daf37d992fdedd538e14895de4001cae
dvargas42 <dvargas.eng@gmail.com>
2021-03-25 18:31:58 -0300

    create global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,45 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --blue: #5429cc;
+    --blue-light: #6933ff;
+    --green: #33cc95;
+    --red: #e62e4D;
+
+    --text-title: #363f5f;
+    --text-body: #969cb3;
+    --background: #f0f2f5;
+    --shape: #ffffff;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    @media (max-width: 1080px){
+      font-size: 93.75%;
+    }
+    @media (max-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
+  body {
+    background: var(---background);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+`;
