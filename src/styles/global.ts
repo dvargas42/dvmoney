5971a88b1940e75490f849e94fb00f3e940a0e4b
dvargas42 <dvargas.eng@gmail.com>
2021-03-26 01:28:10 -0300

    create global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -3,9 +3,10 @@ import { createGlobalStyle } from 'styled-components'
 export const GlobalStyle = createGlobalStyle`
   :root {
     --blue: #5429cc;
-    --blue-light: #6933ff;
     --green: #33cc95;
     --red: #e62e4D;
+
+    --blue-light: #6933ff;
 
     --text-title: #363f5f;
     --text-body: #969cb3;
@@ -32,6 +33,15 @@ export const GlobalStyle = createGlobalStyle`
   body {
     background: var(---background);
     -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
   }
 
   button {
