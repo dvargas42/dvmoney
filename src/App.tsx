aedd1f45bede0ee15bd898ac0f9ff88acbe5c7d3
dvargas42 <dvargas.eng@gmail.com>
2021-03-27 17:27:37 -0300

    include components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
-import { GlobalStyle } from './styles/global'
+import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
     <>
       <Header />
+      <Dashboard />
       <GlobalStyle />
     </>
   );
